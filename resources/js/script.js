98726723620603095a445acdbaa41994ebc3099a
Joshua Noon <josh.noon1@gmail.com>
2018-10-17 21:00:22 +0200

    i can animate with JS

--- resources/js/script.js
+++ resources/js/script.js
@@ -19,6 +19,7 @@ window.onscroll = function() {stickyFunction()};
 
 // Get the header
 var header = document.getElementById("icon-contact");
+var content = document.getElementById("stick-icon-cont");
 
 // Get the offset position of the navbar
 var sticky = header.offsetTop;
@@ -26,7 +27,9 @@ var sticky = header.offsetTop;
 function stickyFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    content.classList.add("shift");
   } else {
     header.classList.remove("sticky");
+    content.classList.remove("shift");
   }
 }
