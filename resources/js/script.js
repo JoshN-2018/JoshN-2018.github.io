e7df261a782b1c7174cf2e53ddb7ae0d3d795619
Joshua Noon <josh.noon1@gmail.com>
2018-10-22 08:39:40 +0200

    drop down header working everything else in ruins

--- resources/js/script.js
+++ resources/js/script.js
@@ -33,3 +33,25 @@ function stickyFunction() {
     content.classList.remove("shift");
   }
 }
+
+
+// Mobile sticky header
+
+window.onscroll = function() {stickyFunctionLarge()};
+
+// Get the header
+var headerLarge = document.getElementById("sticky-contact");
+var gap = document.getElementById("container-case-stick");
+
+// Get the offset position of the navbar
+var stickyLarge = headerLarge.offsetTop;
+
+function stickyFunctionLarge() {
+  if (window.pageYOffset > stickyLarge) {
+    headerLarge.classList.add("sticky");
+    gap.style.marginBottom = "0px";
+  } else {
+    headerLarge.classList.remove("sticky");
+    gap.style.marginBottom = "-196px";
+  }
+}
