29761fdea61182274702f5a0556ececb86e15008
Joshua Noon <josh.noon1@gmail.com>
2018-10-22 20:18:14 +0200

    it fucking works all the headers work they fucken work, boom

--- resources/js/script.js
+++ resources/js/script.js
@@ -17,41 +17,35 @@ function off() {
 window.onscroll = function() {stickyFunction()};
 
 // Get the header
+   // small
 var header = document.getElementById("icon-contact");
 var content = document.getElementById("stick-icon-cont");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-function stickyFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("stickyM");
-    content.classList.add("shift");
-  } else {
-    header.classList.remove("stickyM");
-    content.classList.remove("shift");
-  }
-}
-
-/*
-// Desktop sticky header
-
-window.onscroll = function() {stickyFunctionLarge()};
-
-// Get the header
+   // medium / large
 var headerLarge = document.getElementById("sticky-contact");
 var gap = document.getElementById("container-case-stick");
 
+
 // Get the offset position of the navbar
+   // small
+var sticky = header.offsetTop;
+   // large
 var stickyLarge = headerLarge.offsetTop;
 
-function stickyFunctionLarge() {
-  if (window.pageYOffset > stickyLarge) {
+
+function stickyFunction() {
+  if (window.pageYOffset > sticky || stickyLarge) {
+    // small
+    header.classList.add("stickyM");
+    content.classList.add("shift");
+    // large
     headerLarge.classList.add("sticky");
     gap.style.marginBottom = "0px";
   } else {
+    // small
+    header.classList.remove("stickyM");
+    content.classList.remove("shift");
+    // large
     headerLarge.classList.remove("sticky");
     gap.style.marginBottom = "-196px";
   }
 }
-*/
