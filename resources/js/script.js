00293cfd93235ecba4dd1f878b1b249d7ba8dd6c
Joshua Noon <josh.noon1@gmail.com>
2018-11-03 15:18:32 +0100

    transform translate logic working, no animation yet

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,15 +1,19 @@
 // Overlay functionality
 
+var overlay = document.getElementById("overlay");
+
 function on() {
-    document.getElementById("overlay").style.display = "block";
-    document.getElementById("menu-button").style.display = "none";
-     document.getElementById("menu-button-off").style.display = "block";
+   overlay.classList.add("overlayOn");
+    // document.getElementById("overlay").style.display = "block";
+   document.getElementById("menu-button").style.display = "none";
+   document.getElementById("menu-button-off").style.display = "block";
 }
 
 function off() {
-    document.getElementById("overlay").style.display = "none";
-    document.getElementById("menu-button-off").style.display = "none";
-     document.getElementById("menu-button").style.display = "block";
+   overlay.classList.remove("overlayOn");
+   // document.getElementById("overlay").style.display = "none";
+   document.getElementById("menu-button-off").style.display = "none";
+   document.getElementById("menu-button").style.display = "block";
 }
 
 // Mobile sticky header
